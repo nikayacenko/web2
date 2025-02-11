bca6fe6a9b47aecebd776b77d76af511858527d3
nikayacenko <yacenkonika05@mail.ru>
2025-02-11 19:21:04 +0300

    Update index.php

--- lab3/project4/projects/1/index.php
+++ lab3/project4/projects/1/index.php
@@ -88,9 +88,9 @@ $user = 'u68600'; // Заменить на ваш логин uXXXXX
 $pass = '8589415'; // Заменить на пароль
 $db = new PDO('mysql:host=localhost;dbname=u68600', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-&table_app = 'applucation';
-&table_lang = 'prog_lang';
-&table_ul = 'user_lang';
+$table_app = 'applucation';
+$table_lang = 'prog_lang';
+$table_ul = 'user_lang';
 
 // Подготовленный запрос. Не именованные метки.
 try {
