d4489d6ec8f457e4288ef5de708bab6691f0ebd0
nikayacenko <yacenkonika05@mail.ru>
2025-02-17 18:58:51 +0300

    Update form.php

--- lab4/form.php
+++ lab4/form.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>зомбиробоповарозавр</title>
-        <link href = "static/styles/style.css" rel = "stylesheet">
+        <link href = "project4/projects/1/static/styles/style.css" rel = "stylesheet">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <link rel="stylesheet"
