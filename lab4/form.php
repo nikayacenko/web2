bab776045182d0909a3492944090e18d7a73bde3
nikayacenko <yacenkonika05@mail.ru>
2025-02-18 20:16:35 +0300

    Update form.php

--- lab4/form.php
+++ lab4/form.php
@@ -93,8 +93,7 @@
                             <label>
                                 Любимый язык программирования:
                                 <br>
-                                <select name="languages[]" <?php if ($errors['languages']) {print 'class="error"';} ?>
-                                multiple="multiple">
+                                <select name="languages[]" multiple="multiple" <?php if ($errors['languages']) {print 'class="error"';} ?>>
                                 <option value="Pascal" <?php if(in_array('Pascal', $user_languages)) {print 'selected="selected"';}?>>Pascal</option>
                                 <option value="C" <?php if(in_array('C', $user_languages)) {print 'selected="selected"';}?>>C
                                 <option value="C++" <?php if(in_array('C++', $user_languages)) {print 'selected="selected"';}?>>C++
