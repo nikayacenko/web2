6bd29c6b22241856d59bc5535dae461eedd6f56e
nikayacenko <yacenkonika05@mail.ru>
2025-03-02 22:33:21 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -2,9 +2,9 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-  $user = 'u68598'; // Заменить на ваш логин uXXXXX
-  $pass = '8795249'; // Заменить на пароль
-  $db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
+  $user = 'u68600'; // Заменить на ваш логин uXXXXX
+  $pass = '8589415'; // Заменить на пароль
+  $db = new PDO('mysql:host=localhost;dbname=u68600', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
