b12d1a9197a17d11d1e9c1005b0fcccb12f91e42
nikayacenko <yacenkonika05@mail.ru>
2025-03-02 22:55:37 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -338,9 +338,9 @@ else{
       print('Error : ' . $e->getMessage());
       exit();
     }
-    try{
-      $login = /*rand()%10000000*/123;
-      $pass = /*rand()%10000000000*/123;
+    /*try{
+      $login = rand()%10000000;
+      $pass = rand()%10000000000;
       // Сохраняем в Cookies.
       $hash_pass=md5($pass);
       setcookie('login', $login);
@@ -357,7 +357,7 @@ else{
       catch(PDOException $e){
       print('Error : ' . $e->getMessage());
       exit();
-    }
+    }*/
     $user_id = $db->lastInsertId(); // ID последнего вставленного пользователя
     try{
       $stmt = $db->prepare("SELECT id_lang_name FROM prog WHERE lang_name = ?");
