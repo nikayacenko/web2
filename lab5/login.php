5e2f72a53a637b6c2e6782d5899a349f399c416f
nikayacenko <yacenkonika05@mail.ru>
2025-03-04 13:47:36 +0300

    Update login.php

--- lab5/login.php
+++ lab5/login.php
@@ -11,17 +11,46 @@
 // Отправляем браузеру правильную кодировку,
 // файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
+function isValid($login, $db) {
+  $count;
+  try{
+    $stmt = $db->prepare("SELECT COUNT(*) FROM person_LOGIN WHERE login = ?");
+    $stmt->execute([$login]);
+    $count = $stmt->fetchColumn();
+  } 
+  catch (PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
+  return $count > 0;
+}
 
+function password_check($login, $password, $db) {
+  $passw;
+  try{
+    $stmt = $db->prepare("SELECT pass FROM LOGIN WHERE login = :login");
+    $stmt->bindParam(':login', $login, PDO::PARAM_STR);
+    $stmt->execute();
+    $passw = $stmt->fetchColumn();
+  } 
+  catch (PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
+  return ($passw==$password);
+}
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.
 $session_started = false;
 if ($_COOKIE[session_name()] && session_start()) {
   $session_started = true;
   if (!empty($_SESSION['login'])) {
-    // Если есть логин в сессии, то пользователь уже авторизован.
-    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
-    //при нажатии на кнопку Выход).
-    // Делаем перенаправление на форму.
+    if(isset($_POST['logout'])){
+      session_unset();
+      session_destroy();
+      header('Location: login.php');
+      exit();
+    }
     header('Location: ./');
     exit();
   }
@@ -42,27 +71,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 // Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
-  // TODO: Проверть есть ли такой логин и пароль в базе данных.
-  // Выдать сообщение об ошибках.
+   $login = $_POST['login'];
+  $password = md5($_POST['pass']);
+
+  $user = 'u68600';
+  $pass = '8589415';
+  $db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
   if (!$session_started) {
     session_start();
   }
-  // Если все ок, то авторизуем пользователя.
-  $_SESSION['login'] = $_POST['login'];
-  // Записываем ID пользователя.
-  // Записываем ID пользователя.
-//   $user = 'u68598'; // Заменить на ваш логин uXXXXX
-//   $pass = '8795249'; // Заменить на пароль
-//   $db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
-//     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-//     $stmt = $db->prepare("SELECT id FROM person_LOGIN WHERE login=:login");
-//     $stmt->bindValue(':login', $_SESSION['login'], PDO::PARAM_STR);
-//     if (!$stmt->execute()) {
-//         error_log("Ошибка выполнения запроса: " . $stmt->errorInfo()[2]); 
-//       }
-  $_SESSION['uid'] = 123;
+  if (isValid($login, $db) && password_check($login, $password, $db)){
+    $_SESSION['login'] = $_POST['login'];
 
-  // Делаем перенаправление.
-  header('Location: ./');
+    $_SESSION['uid'];
+    try {
+        $stmt_select = $db->prepare("SELECT id FROM person_LOGIN WHERE login=?");
+        $stmt_select->execute([$_SESSION['login']]);
+        $_SESSION['uid']  = $stmt_select->fetchColumn();
+    } catch (PDOException $e){
+        print('Error : ' . $e->getMessage());
+        exit();
+    }
+
+    // Делаем перенаправление.
+    header('Location: ./');
+  }
+  else {
+    print('Неверный логин или пароль');
+  }
 }
