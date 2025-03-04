7c74342d51b88ec872ec2163d42429d51e155778
nikayacenko <yacenkonika05@mail.ru>
2025-03-04 10:26:59 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -178,7 +178,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
     /*$sql = "SELECT pl.lang_id FROM personlang pl JOIN person_LOGIN l ON pl.pers_id = l.id  WHERE l.login = :login;";*/
     try{
-      $stmt = $db->prepare("select pl.lang_name from prog_lang pl JOIN prog_lang ul ON pl.id_lang_name=ul.id_lang_name where ul.id = :login;");
+      $stmt = $db->prepare("select pl.lang_name from prog pl JOIN prog_lang ul ON pl.id_lang_name=ul.id_lang_name where ul.id = :login;");
       $stmt->bindValue(':login', $_SESSION['login'], PDO::PARAM_STR);
       $stmt->execute();
       $lang = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
