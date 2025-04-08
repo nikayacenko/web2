c933b9a32ac0da4de6ef01a9bb67d7883f26bbc8
yacenkonika <yacenkonika05@mail.ru>
2025-04-09 02:28:40 +0300

    c

--- lab6/index.php
+++ lab6/index.php
@@ -262,9 +262,8 @@ else{
   }
 
 
-  if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW']) && $_SERVER['PHP_AUTH_USER'] ==  admin_login_check($db) && admin_password_check($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $db)){
-    error_log("Authentication successful!");
-    header('Location: login.php');
+  //if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW']) && $_SERVER['PHP_AUTH_USER'] ==  admin_login_check($db) && admin_password_check($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $db)){
+    //error_log("Authentication successful!");
     if(!empty($_POST['uid'])) {
       $user_id = $_POST['uid'];
       $lang = $_POST['languages'] ?? [];
@@ -275,91 +274,7 @@ else{
     } else{
       print('Пользователь для изменения не выбран');
     }
-  }
-  else{
-    if (isset($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login'])) {
-      $user_id;
-      try {
-          $stmt_select = $db->prepare("SELECT id FROM person_LOGIN WHERE login=?");
-          $stmt_select->execute([$_SESSION['login']]);
-          $user_id = $stmt_select->fetchColumn();
-      } catch (PDOException $e){
-          print('Error : ' . $e->getMessage());
-          exit();
-      }
 
-      //update
-      try {
-          $stmt_update = $db->prepare("UPDATE application SET name=?, number=?, email=?, bdate=?, gender=?, biography=?, checkbox=? WHERE id=?");
-          $stmt_update->execute([$_POST['name'], $_POST['number'], $_POST['email'], $_POST['bdate'], $_POST['gender'], $_POST['biography'], isset($_POST["checkbox"]) ? 1 : 0, $user_id ]);
-      
-          $stmt_delete = $db->prepare("DELETE FROM prog_lang WHERE id=?");
-          $stmt_delete -> execute([$user_id]);
-          $stmt_select = $db->prepare("SELECT id_lang_name FROM prog WHERE lang_name = ?");
-          $insert_stmt = $db->prepare("INSERT INTO prog_lang (id, id_lang_name) VALUES (?,?)");
-          foreach ($fav_languages as $language) {
-            $stmt_select->execute([$language]);
-            $language_id = $stmt_select->fetchColumn();
-            
-            if ($language_id) {
-                $insert_stmt->execute([$user_id, $language_id]);
-            }
-          }
-      } catch (PDOException $e){
-          print('update Error : ' . $e->getMessage());
-          exit();
-      }
-
-    } 
-    else{
-      try {
-        $stmt = $db->prepare("INSERT INTO application(name, number, email, gender, bdate, biography, checkbox) values(?,?,?,?,?,?,?)");
-        $stmt->execute([$_POST['name'], $_POST['number'], $_POST['email'], $_POST['gender'], $_POST['bdate'], $_POST['biography'], isset($_POST["checkbox"]) ? 1 : 0]);
-      }
-      catch(PDOException $e){
-        print('Error : ' . $e->getMessage());
-        exit();
-      }
-      $user_id = $db->lastInsertId(); 
-      try{
-        //$characters = '0123456789';
-        $login = rand()%10000000;
-        //$pass = substr(str_shuffle($characters), 0, 10); //uniqid(string $prefix = "", bool $more_entropy = false);
-        $pass = generate_pass();
-        $hash_p = password_hash($pass, PASSWORD_DEFAULT);
-        setcookie('login', $login);
-        setcookie('pass', $pass);
-        $stmt = $db->prepare("INSERT INTO LOGIN (login, pass) VALUES (:login, :pass)");
-        $stmt->bindParam(':login', $login);
-        $stmt->bindParam(':pass', $hash_p);
-        $stmt->execute();
-        $stmt = $db->prepare("INSERT INTO person_LOGIN (id, login) VALUES (:id, :login)");
-        $stmt->bindParam(':id', $user_id);
-        $stmt->bindParam(':login', $login);
-        $stmt->execute();
-      }
-        catch(PDOException $e){
-        print('Error : ' . $e->getMessage());
-        exit();
-      }
-      try{
-        $stmt = $db->prepare("SELECT id_lang_name FROM prog WHERE lang_name = ?");
-        $insert_stmt = $db->prepare("INSERT INTO prog_lang (id, id_lang_name) VALUES (?, ?)");
-        
-        foreach ($fav_languages as $language) {
-            $stmt->execute([$language]);
-            $language_id = $stmt->fetchColumn();
-            if ($language_id) {
-                $insert_stmt->execute([$user_id, $language_id]);
-            }
-        }
-      }
-      catch (PDOException $e) {
-        print('Ошибка БД: ' . $e->getMessage());
-        exit();
-      }
-    }
-}
   setcookie('save', '1');
   header('Location: index.php');
 }
